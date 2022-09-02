37979ecd24d8e75b4827a57f216fa0c88a4a16c8
mahir <mahirul.lasker@northsouth.edu>
2022-09-02 14:21:36 +0600

    admin panel changes implemented

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -4,6 +4,6 @@
         <li><i class="fas fa-ticket-alt"></i><a href="view.php">Bookings</a> <i class="fas admin-dropdown fa-chevron-right"></i></li>
         
         <li><i class="fas fa-film"></i><a href="addmovie.php">Movies</a> <i class="fas admin-dropdown fa-chevron-right"></i></a></li>
-        <li><i class="fas fa-plus-circle"></i><a href="add.php">Add entry</a> <i class="fas admin-dropdown fa-chevron-right"></i></a></li>
+        
         
 </div>
